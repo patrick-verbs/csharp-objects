26049a914c8b341dde946fd294e714ce68c6b3aa
Patrick Lee <patricklee1138@live.com>
2021-05-02 15:05:26 -0700

    :tada: initial commit
    Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com>
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

---
+++ Car.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+public class Car
+{
+  public string MakeModel;
+  public int Price;
+  public int Miles;
+}
+
+public class Program
+{
+  public static void Main()
+  {
+    Car volkswagen = new Car();
+    volkswagen.MakeModel = "1974 Volkswagen Thing";
+    volkswagen.Price = 1100;
+    volkswagen.Miles = 368792;
+
+    Car yugo = new Car();
+    yugo.MakeModel = "1980 Yugo Koral";
+    yugo.Price = 700;
+    yugo.Miles = 56000;
+
+    Car ford = new Car();
+    ford.MakeModel = "1988 Ford Country Squire";
+    ford.Price = 1400;
+    ford.Miles = 239001;
+
+    Car amc = new Car();
+    amc.MakeModel = "1976 AMC Pacer";
+    amc.Price = 400;
+    amc.Miles = 198000;
+  }
+}
